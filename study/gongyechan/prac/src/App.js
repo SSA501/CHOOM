a694f063c5cd23cacb17831ad3a3db6cc8652947
Yechan Gong <rhddpcks9814@naver.com>
2023-03-08 17:32:58 +0900

    Feat: Add React 비디오 녹화 페이지 [S08P22A501-81]

--- study/gongyechan/prac/src/App.js
+++ study/gongyechan/prac/src/App.js
@@ -1,17 +1,15 @@
-import PropTypes from 'prop-types';
-import './assets/css/App.css';
+import PropTypes from "prop-types";
+import "./assets/css/App.css";
 
 const App = (props) => {
   return (
     <div className="App">
-      <header>테스트 페이지</header>
-      <div className="container">
-        {props.children}
-      </div>
+      <header><h1>예찬이가 만든 페이지</h1></header>
+      <div className="container">{props.children}</div>
       <footer>Copyright Yechan. All right reserved.</footer>
     </div>
   );
-}
+};
 
 App.propTypes = {
   children: PropTypes.element.isRequired,
--- study/gongyechan/prac/src/pages/VideoView.js
+++ study/gongyechan/prac/src/pages/VideoView.js
@@ -1,78 +1,82 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
+import "../assets/css/VideoView.css";
 
 const VideoView = () => {
-  const videoRealtime = React.useRef();
-  const videoRecord = React.useRef();
+  const videoRealtime = useRef();
+  const videoRecord = useRef();
+  const btnDownload = useRef();
+  const mediaRecorder = useRef(null);
   
-  const [recording, setRecording] = React.useState(false);
+  const [recording, setRecording] = useState(false);
   
-  var mediaRecorder = null;
   var arrVideoData = [];
-
   
   // 녹화 시작
   const startRecord = (e) => {
+    setRecording(true);
     // 카메라 입력영상 스트림 생성
-    const mediaStream = navigator.mediaDevices.getUserMedia({
-      audio: true,
-      video: true
-    });
-    
-    // 실시간 영상 재생 처리: 첫번째 video태그에서 재생
-    videoRealtime.current.srcObject = mediaStream;
-    videoRealtime.current.onloadedmetadata = () => {
-      videoRealtime.current.play();
-    }
+    const constraints = { audio: true, video: true };
+    navigator.mediaDevices.getUserMedia(constraints).then((mediaStream) => {
+      // 실시간 영상 재생 처리: 첫번째 video태그에서 재생
+      videoRealtime.current.srcObject = mediaStream;
+      // videoRealtime.current.src = mediaStream;
+      videoRealtime.current.onloadedmetadata = () => {
+        videoRealtime.current.play();
+      };
 
-    // mediaRecorder객체(녹화기) 생성
-    mediaRecorder = new MediaRecorder(mediaStream);
+      // mediaRecorder객체(녹화기) 생성
+      mediaRecorder.current = new MediaRecorder(mediaStream);
 
-    // 녹화 데이터 입력 이벤트 처리
-    mediaRecorder.ondataavailable = (event)=>{
+      // 녹화 데이터 입력 이벤트 처리
+      mediaRecorder.current.ondataavailable = (event) => {
         // 녹화 데이터(Blob)가 들어올 때마다 배열에 담아두기
         arrVideoData.push(event.data);
-    }
+      };
 
-    // 녹화 종료 이벤트 처리
-    mediaRecorder.onstop = () => {
+      // 녹화 종료 이벤트 처리
+      mediaRecorder.current.onstop = () => {
         // 배열에 담아둔 녹화 데이터들을 통합한 Blob객체 생성
         const videoBlob = new Blob(arrVideoData);
 
         // BlobURL(ObjectURL) 생성
         const blobURL = window.URL.createObjectURL(videoBlob);
-        
+
         // 녹화된 영상 재생: 두번째 video태그에서 재생
         videoRecord.current.src = blobURL;
         videoRecord.current.play();
-        
+
         // 기존 녹화 데이터 제거
         arrVideoData.splice(0);
-        
-    }
-  
-    // 녹화 시작
-    setRecording(true);
-    mediaRecorder.start();
-  }
-  
+      };
+
+      // 녹화 시작
+      mediaRecorder.current.start();
+    });
+  };
+
   // 녹화 종료
   const stopRecord = (e) => {
     setRecording(false);
-    mediaRecorder.stop();
+    mediaRecorder.current.stop();
+  };
+
+  // 다운로드
+  const downloadRecord = (e) => {
+    btnDownload.current.href = videoRecord.current.src;
+    btnDownload.current.download = "record_video.mp3";
   }
-  
+
   return (
     <div className="video-container">
-      <p>[비디오]</p>    
-      <video ref={videoRealtime} controls>실시간 영상 재생용</video>
-      <video ref={videoRecord} controls>녹화된 영상 재생용</video>
-      <br/><br/>
-      <button onClick={startRecord}>시작</button>
-      <button onClick={stopRecord}>정지</button>
+      <h2>Video Recorder</h2>
+      {recording && <video ref={videoRealtime} />}
+      {!recording && <video ref={videoRecord} autoplay="autoplay" loop="loop" muted="muted" playsinline=""/> }
+      <br />
+      <br />
+      <button onClick={recording ? stopRecord: startRecord}>{recording ? "정지" : "시작"}</button>
+      {!recording && <a ref={btnDownload} onClick={downloadRecord}><button>다운로드</button></a>}
     </div>
   );
-}
-
-
+};
 
 export default VideoView;
