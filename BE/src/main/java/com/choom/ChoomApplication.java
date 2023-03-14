f681c8d1395e604cea5b5b1cb4dcd4aa20ed8283
jangyejoo <jyj01068662778@gamil.com>
2023-03-14 13:38:05 +0900

    Feat : backend project init (#1)

---
+++ BE/src/main/java/com/choom/ChoomApplication.java
@@ -0,0 +1,16 @@
+package com.choom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration .class})
+public class ChoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoomApplication.class, args);
+	}
+
+}
