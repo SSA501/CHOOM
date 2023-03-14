d6585481718a792a55226efc23539a2c9536b81e
김경림 <myway01212@naver.com>
2023-03-14 17:29:31 +0900

    Add: 레이아웃 컴포넌트 추가(#S08P22A501-238)

--- FE/frontend/src/App.tsx
+++ FE/frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
 import MainPage from "./pages/MainPage/MainPage";
+import ChallengePage from "./pages/ChallengePage/ChallengePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
     <Routes>
-      <Route index element={<MainPage />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<MainPage />} />
+        <Route path="/challenge" element={<ChallengePage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Route>
     </Routes>
   );
 }
