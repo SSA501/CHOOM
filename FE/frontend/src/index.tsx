5b243ba9985ca10fb8abf98ebeeac59276523d06
김경림 <myway01212@naver.com>
2023-03-14 15:22:21 +0900

    Add: 공통 스타일 변수 및 폰트 설정 (#S08P22A501-188)

--- FE/frontend/src/index.tsx
+++ FE/frontend/src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./styles/globalStyle";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
---
+++ FE/frontend/src/styles/globalStyle.ts
@@ -0,0 +1,64 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  @font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  :root {
+    /* Colors */
+    --blue-color: #08439D;
+    --green-color: #00A25E;
+    --yellow-color: #FFE550;
+    --pink-color: #F7B5CE;
+    --orange-color: #EB6D15;
+    --red-color: #EB6D15;
+  }
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+  
+  html, body {
+    color: var(--default-font-color);
+    width: 100%;
+    height: 100%;
+    font-family: 'Pretendard-Regular', -apple-system, Helvetica Neue, sans-serif;
+  }
+  button {
+    border: none;
+    cursor: pointer;
+    background-color: transparent;
+    padding: 0;
+  }
+  input {
+    border: none;
+    background-color: inherit;
+  }
+  input:focus {
+    outline: none;
+  }
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+  abbr {
+    text-decoration: none;
+  }
+  a,
+  div,
+  span,
+  input,
+  button,
+  textarea {
+    font-family: inherit;
+  }
+`
+
+export {GlobalStyle}
