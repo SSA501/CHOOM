4419e1ab217d348ba2dc65fc45090e1cf798e962
tykwon97 <tykwon97@naver.com>
2023-03-14 15:40:06 +0900

    Feat : JPA Auditing 설정 (#1)

--- BE/src/main/java/com/choom/ChoomApplication.java
+++ BE/src/main/java/com/choom/ChoomApplication.java
@@ -2,11 +2,10 @@ package com.choom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class, HibernateJpaAutoConfiguration .class})
+@EnableJpaAuditing
+@SpringBootApplication
 public class ChoomApplication {
 
 	public static void main(String[] args) {
