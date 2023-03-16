67fcf4a74fc036fdcd0c1da4a099281c64f7d54d
김경림 <myway01212@naver.com>
2023-03-16 10:37:49 +0900

    Add: 비디오 컴포넌트 기본 골격 및 마우스 오버시 자동재생 (#S08P22A501-234)

---
+++ FE/frontend/src/components/Video/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+const VideoContainer = styled.div`
+  width: 334px;
+  height: 594px;
+`;
+
+export { VideoContainer };
--- FE/frontend/src/pages/MainPage/MainPage.tsx
+++ FE/frontend/src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Video from "../../components/Video/Video";
 
 function MainPage() {
-  return <div>메인페이지</div>;
+  const videoData = [
+    {
+      id: 1,
+      videoSrc: "https://www.youtube.com/shorts/fYQxthUKung",
+      thumbnailSrc:
+        "https://i.ytimg.com/vi/fYQxthUKung/hq720_2.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLCWglyEsDggRm3EeUuUFrcT5b7iBA",
+    },
+  ];
+  return (
+    <div>
+      메인페이지
+      {videoData.map(({ id, videoSrc, thumbnailSrc }) => (
+        <Video id={id} videoSrc={videoSrc} thumbnailSrc={thumbnailSrc} />
+      ))}
+    </div>
+  );
 }
 
 export default MainPage;
