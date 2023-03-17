2bac10d211e0a49a8ac57741ec608667a2e3df84
tykwon97 <tykwon97@naver.com>
2023-03-17 17:52:19 +0900

    Feat: User JPA Method(getUserById, getUserByIdentifier)

--- BE/src/main/java/com/choom/domain/user/service/UserService.java
+++ BE/src/main/java/com/choom/domain/user/service/UserService.java
@@ -1,7 +1,9 @@
 package com.choom.domain.user.service;
 
 import com.choom.domain.user.entity.User;
+import java.util.Optional;
 
 public interface UserService {
-    User getUserByUserId(String userId);
+    Optional<User> getUserById(Long userId);
+    Optional<User> getUserByIdentifier(String identifier);
 }
