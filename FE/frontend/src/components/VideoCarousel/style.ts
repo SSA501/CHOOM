873f60ee2a98f4b825ad2313bdeaa7ea96ca1410
김경림 <myway01212@naver.com>
2023-03-20 10:56:42 +0900

    Add: 메인 인기 챌린지 스타일 구현 (#S08P22A501-314)

--- FE/frontend/src/components/VideoCarousel/style.ts
+++ FE/frontend/src/components/VideoCarousel/style.ts
@@ -4,6 +4,21 @@ const SwiperContainer = styled.div`
   padding: 5em;
   width: 80%;
   margin: 0 auto;
+  h2 {
+    text-align: center;
+    font-weight: 600;
+    font-size: 1.7em;
+    padding: 1.2em 1em;
+  }
 `;
 
-export { SwiperContainer };
+const InnerContainer = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+
+const ArrowBtn = styled.button`
+  font-size: 2em;
+`;
+
+export { SwiperContainer, InnerContainer, ArrowBtn };
