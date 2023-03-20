3c9c3134304e1504b23135839ee352785bb2c76d
김경림 <myway01212@naver.com>
2023-03-20 13:59:41 +0900

    Feat: 메인 배너 자동 캐러셀 기능 구현(#S08P22A501-306)

--- FE/frontend/src/components/MainBanner/MainBanner.tsx
+++ FE/frontend/src/components/MainBanner/MainBanner.tsx
@@ -4,22 +4,67 @@ import {
   BannerIconsContainer,
   BannerTextContainer,
 } from "./style";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, A11y, Autoplay } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function MainBanner() {
   return (
-    <BannerContainer>
-      <BannerTextContainer>
-        <p>지금 무슨 노래 듣고 계세요?</p>
-        <p>C#OOM과 함께라면 유행하는 챌린지 뚝딱!</p>
-      </BannerTextContainer>
-      <BannerIconsContainer>
-        <img
-          style={{ width: "100%" }}
-          src="/assets/icons_main_shorts_tiktok.png"
-          alt="쇼츠, 틱톡 아이콘"
-        />
-      </BannerIconsContainer>
-    </BannerContainer>
+    <Swiper
+      modules={[Navigation, Pagination, Autoplay, A11y]}
+      loop={true}
+      slidesPerView={1}
+      pagination={true}
+      autoplay={true}
+    >
+      <SwiperSlide>
+        <BannerContainer>
+          <BannerTextContainer>
+            <p>지금 무슨 노래 듣고 계세요?</p>
+            <p>C#OOM과 함께라면 유행하는 챌린지 뚝딱!</p>
+          </BannerTextContainer>
+          <BannerIconsContainer>
+            <img
+              style={{ width: "100%" }}
+              src="/assets/icons_main_shorts_tiktok.png"
+              alt="쇼츠, 틱톡 아이콘"
+            />
+          </BannerIconsContainer>
+        </BannerContainer>
+      </SwiperSlide>
+      <SwiperSlide>
+        <BannerContainer>
+          <BannerTextContainer>
+            <p>2번문구</p>
+            <p>C#OOM과 함께라면 유행하는 챌린지 뚝딱!</p>
+          </BannerTextContainer>
+          <BannerIconsContainer>
+            <img
+              style={{ width: "100%" }}
+              src="/assets/icons_main_shorts_tiktok.png"
+              alt="쇼츠, 틱톡 아이콘"
+            />
+          </BannerIconsContainer>
+        </BannerContainer>
+      </SwiperSlide>
+      <SwiperSlide>
+        <BannerContainer>
+          <BannerTextContainer>
+            <p>3번문구</p>
+            <p>C#OOM과 함께라면 유행하는 챌린지 뚝딱!</p>
+          </BannerTextContainer>
+          <BannerIconsContainer>
+            <img
+              style={{ width: "100%" }}
+              src="/assets/icons_main_shorts_tiktok.png"
+              alt="쇼츠, 틱톡 아이콘"
+            />
+          </BannerIconsContainer>
+        </BannerContainer>
+      </SwiperSlide>
+    </Swiper>
   );
 }
 
