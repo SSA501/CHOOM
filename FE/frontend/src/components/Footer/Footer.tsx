29d932979ee5be41711e840746546ae0e5533b71
김경림 <myway01212@naver.com>
2023-03-20 15:43:40 +0900

    Feat: 하단 푸터 구현 및 전체 페이지 스타일 수정 (#S08P22A501-308)

---
+++ FE/frontend/src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterContainer } from "./style";
+
+function Footer() {
+  return (
+    <FooterContainer>
+      <p>Copyright © 2023 C#OOM All rights reserved.</p>
+    </FooterContainer>
+  );
+}
+
+export default Footer;
---
+++ FE/frontend/src/components/Footer/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  height: 80px;
+  background-color: #4e4e4e;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: #d9d9d9;
+  font-size: 14px;
+`;
+export { FooterContainer };
