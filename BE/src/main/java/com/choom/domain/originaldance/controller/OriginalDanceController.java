3a155093dcb3a689e2918b82019391b86b6fee3f
tykwon97 <tykwon97@naver.com>
2023-03-21 11:38:19 +0900

    Refactor: @Autowired 삭제, @RequiredArgsConstructor 추가

--- BE/src/main/java/com/choom/domain/originaldance/controller/OriginalDanceController.java
+++ BE/src/main/java/com/choom/domain/originaldance/controller/OriginalDanceController.java
@@ -4,9 +4,13 @@ import com.choom.domain.originaldance.dto.YoutubeResponseDto;
 import com.choom.domain.originaldance.service.OriginalDanceService;
 import com.choom.global.common.BaseResponse;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +18,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/challenge")
 @Slf4j
+@RequiredArgsConstructor
 public class OriginalDanceController {
 
-    @Autowired
-    OriginalDanceService originalDanceService;
+    private final OriginalDanceService originalDanceService;
 
     @GetMapping()
     public BaseResponse searchChallenge(@RequestParam(name = "q")String keyword, @RequestParam int page){
         log.info("keyword : "+keyword+" page : "+page);
         List<YoutubeResponseDto> youtubeResponseDtos = originalDanceService.searchChallenge(keyword,page);
         return BaseResponse.success(youtubeResponseDtos);
+    }
+
+    @PostMapping()
+    public BaseResponse searchChallenge(@PathVariable Long originalDanceId){
+        log.info("originalDanceId : "+originalDanceId);
+        return BaseResponse.success(null);
     }
 }
