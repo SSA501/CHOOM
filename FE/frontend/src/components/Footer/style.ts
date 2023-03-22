93808a8a17f0e189ff753a2a05f5f4250cecaa61
김경림 <myway01212@naver.com>
2023-03-22 15:56:41 +0900

    Design: 네비바 및 푸터 디자인 수정 (#S08P22A501-317)

--- FE/frontend/src/components/Footer/style.ts
+++ FE/frontend/src/components/Footer/style.ts
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 const FooterContainer = styled.footer`
   height: 80px;
-  background-color: #4e4e4e;
+  background-color: black;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  color: #d9d9d9;
+  color: white;
   font-size: 14px;
   z-index: 99;
 `;
