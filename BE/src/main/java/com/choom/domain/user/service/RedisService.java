9aadcb55572f02bfc8796d9ae2665dd3f660ccc2
yujj25 <yujj25@naver.com>
2023-03-23 09:27:29 +0900

    Update: @Transactional(readOnly = true) 추가 (#9, #S08P22A501-287)

--- BE/src/main/java/com/choom/domain/user/service/RedisService.java
+++ BE/src/main/java/com/choom/domain/user/service/RedisService.java
@@ -7,9 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
-@Slf4j
+@Transactional(readOnly = true)
 public class RedisService {
 
     private final RefreshTokenRedisRepository refreshTokenRedisRepository;
