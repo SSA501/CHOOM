06f7037d250d6d8427727920127567c6ec9b7708
yujj25 <yujj25@naver.com>
2023-03-24 10:09:20 +0900

    Update: UserService 메서드 정리 (#19, #S08P22A501-283)

--- BE/src/main/java/com/choom/domain/user/service/UserService.java
+++ BE/src/main/java/com/choom/domain/user/service/UserService.java
@@ -1,6 +1,5 @@
 package com.choom.domain.user.service;
 
-import com.choom.domain.user.entity.SocialType;
 import com.choom.domain.user.entity.User;
 import com.choom.domain.user.entity.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,15 +15,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public Optional<User> findUserById(Long id) {
-        return userRepository.findById(id);
-    }
-
     public Optional<User> findUserByIdentifier(String identifier) {
         return userRepository.findByIdentifier(identifier);
-    }
-
-    public Optional<User> findUserByIdentifierAndSocialType(String identifier, SocialType socialType) {
-        return userRepository.findByIdentifierAndSocialType(identifier, socialType);
     }
 }
