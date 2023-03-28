679337116507d316daad905a6a1d9b34d465deca
yujj25 <yujj25@naver.com>
2023-03-28 13:30:31 +0900

    Update: UserMyDanceDto @AllArgsConstructor 추가 (#24 , #S08P22A501-331)

--- BE/src/main/java/com/choom/domain/user/dto/UserMyDanceDto.java
+++ BE/src/main/java/com/choom/domain/user/dto/UserMyDanceDto.java
@@ -1,19 +1,15 @@
 package com.choom.domain.user.dto;
 
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserMyDanceDto {
     private Long challengeCount;
     private Integer challengeTime;
     private Double averageScore;
-
-    public UserMyDanceDto(Long challengeCount, Integer challengeTime, Double averageScore) {
-        this.challengeCount = challengeCount;
-        this.challengeTime = challengeTime;
-        this.averageScore = averageScore;
-    }
 }
