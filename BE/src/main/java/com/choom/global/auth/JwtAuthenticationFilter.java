9ba371340208d2c210a1a42ec40070662e2de8a8
yujj25 <yujj25@naver.com>
2023-03-29 13:09:11 +0900

    Update: token 재발급 요청 시 filter 예외 처리

--- BE/src/main/java/com/choom/global/auth/JwtAuthenticationFilter.java
+++ BE/src/main/java/com/choom/global/auth/JwtAuthenticationFilter.java
@@ -7,6 +7,7 @@ import com.choom.domain.user.service.AuthService;
 import com.choom.domain.user.service.UserService;
 import com.choom.global.util.JwtTokenUtil;
 import com.choom.global.util.ResponseBodyWriteUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -19,9 +20,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
 /**
  * 요청 헤더에 jwt 토큰이 있는 경우, 토큰 검증 및 인증 처리 로직 정의.
  */
+@Slf4j
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private UserService userService;
     private AuthService authService;
@@ -41,6 +44,12 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
             return;
         }
         try {
+            String uri = request.getRequestURI();
+            log.info("uri : " + uri);
+            if (uri.equals("/api/user/login/token")) {
+                filterChain.doFilter(request, response);
+                return;
+            }
             // If header is present, try grab user principal from database and perform authorization
             Authentication authentication = getAuthentication(request);
             // jwt 토큰으로 부터 획득한 인증 정보(authentication) 설정.
@@ -66,7 +75,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
             if (identifier != null) {
                 // jwt 토큰에 포함된 계정 정보(userId) 통해 실제 디비에 해당 정보의 계정이 있는지 조회.
                 User user = userService.findUserByIdentifier(identifier).get();
-                if(user != null) {
+                if (user != null) {
                     // 식별된 정상 유저인 경우, 요청 context 내에서 참조 가능한 인증 정보(jwtAuthentication) 생성.
                     CustomUserDetails userDetails = new CustomUserDetails(user);
                     UsernamePasswordAuthenticationToken jwtAuthentication = new UsernamePasswordAuthenticationToken(identifier,
