564127cce81f080f944970b7817fa27c31f4bc15
jangyejoo <jyj01068662778@gamil.com>
2023-03-29 14:03:49 +0900

    Fix: shorts 포스팅 수정

--- BE/src/main/java/com/choom/domain/mydance/service/MyDanceService.java
+++ BE/src/main/java/com/choom/domain/mydance/service/MyDanceService.java
@@ -12,11 +12,8 @@ import com.choom.domain.user.entity.User;
 import com.choom.domain.user.entity.UserRepository;
 import com.choom.global.service.FileService;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.googleapis.media.MediaHttpUploader;
 import com.google.api.client.googleapis.media.MediaHttpUploaderProgressListener;
@@ -25,14 +22,11 @@ import com.google.api.client.http.InputStreamContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Video;
 import com.google.api.services.youtube.model.VideoSnippet;
 import com.google.api.services.youtube.model.VideoStatus;
-import com.google.common.collect.Lists;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -45,6 +39,9 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
 import java.net.UnknownHostException;
 import java.util.*;
 
@@ -71,12 +68,7 @@ public class MyDanceService {
     /* Global instance of the format used for the video being uploaded (MIME type). */
     private static String VIDEO_FILE_FORMAT = "video/*";
 
-    /**
-     * Authorizes the installed application to access user's protected data.
-     *
-     * @param scopes list of scopes needed to run youtube upload.
-     */
-    private static Credential authorize(List<String> scopes) throws Exception {
+    private static Credential authorize(String code) throws Exception {
 
         // Load client secrets.
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
@@ -94,18 +86,58 @@ public class MyDanceService {
         log.info("GOOGLE_CLIENT_ID : "+clientSecrets.getDetails().getClientId());
         log.info("GOOGLE_SECRET : "+clientSecrets.getDetails().getClientSecret());
 
-        // Set up authorization code flow.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, clientSecrets, scopes)
-                .setDataStoreFactory(new FileDataStoreFactory(new File("CREDENTIALS")))
-                .setAccessType("offline")
-                .build();
+        String access_Token="";
+        String reqURL = "https://oauth2.googleapis.com/token";
 
-        // Build the local server and bind it to port 9000
-        LocalServerReceiver localReceiver = new LocalServerReceiver.Builder().setPort(9000).build();
+        try{
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-        // Authorize.
-        return new AuthorizationCodeInstalledApp(flow, localReceiver).authorize("user");
+            //POST 요청을 위해 기본값이 false인 setDoOutput을 true로
+            conn.setRequestMethod("POST");
+            conn.setDoOutput(true);
+
+            //POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
+            StringBuilder sb = new StringBuilder();
+            sb.append("&code=" + code);
+            sb.append("&client_id=" + clientSecrets.getDetails().getClientId());
+            sb.append("&client_secret=" + clientSecrets.getDetails().getClientSecret());
+            sb.append("&redirect_uri=" + "https://j8a501.p.ssafy.io/login/oauth2/google");
+            sb.append("&grant_type=" + "authorization_code");
+            bw.write(sb.toString());
+            bw.flush();
+
+            //결과 코드가 200이라면 성공
+            int responseCode = conn.getResponseCode();
+
+            //요청을 통해 얻은 JSON타입의 Response 메세지 읽어오기
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line = "";
+            String result = "";
+
+            while ((line = br.readLine()) != null) {
+                result += line;
+            }
+
+            //Gson 라이브러리에 포함된 클래스로 JSON파싱 객체 생성
+            JsonParser parser = new JsonParser();
+            JsonElement element = parser.parse(result);
+
+            access_Token = element.getAsJsonObject().get("access_token").getAsString();
+
+            br.close();
+            bw.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return new GoogleCredential.Builder()
+                .setTransport(HTTP_TRANSPORT)
+                .setJsonFactory(JSON_FACTORY)
+                .setClientSecrets(clientSecrets)
+                .build()
+                .setAccessToken(access_Token);
     }
 
     @Transactional
@@ -181,11 +213,11 @@ public class MyDanceService {
     }
 
     @Transactional
-    public AddShortsResponseDto addShorts(Long myDanceId) {
+    public AddShortsResponseDto addShorts(Long myDanceId, String code) {
         MyDance myDance = myDanceRepository.findById(myDanceId)
                 .orElseThrow(() -> new IllegalArgumentException("내 챌린지를 찾을 수 없습니다"));
 
-        String videoId = uploadVideo(myDance.getVideoPath(), myDance.getTitle());
+        String videoId = uploadVideo(myDance.getVideoPath(), myDance.getTitle(), code);
         String youtubeUrl = "https://youtube.com/shorts/"+videoId;
 
         myDance.updateYoutubeUrl(youtubeUrl);
@@ -195,13 +227,11 @@ public class MyDanceService {
                 .build();
     }
 
-    private String uploadVideo(String videoPath, String title) {
-        // Scope required to upload to YouTube.
-        List<String> scopes = Lists.newArrayList("https://www.googleapis.com/auth/youtube.upload");
-
+    private String uploadVideo(String videoPath, String title, String code) {
         try {
             // Authorization.
-            Credential credential = authorize(scopes);
+            Credential credential = authorize(code);
+            log.info("token : " + credential.getAccessToken());
 
             // 인증 정보를 파일로 저장합니다.
             saveCredentialsToFile(credential);
@@ -211,6 +241,12 @@ public class MyDanceService {
                     "Choom").build();
 
             // We get the user selected local video file to upload.
+            String hostname = InetAddress.getLocalHost().getHostName();
+            if (hostname.substring(0, 7).equals("DESKTOP")) {
+                videoPath = "C:" + videoPath;
+            } else {
+                videoPath = "/var/lib" + videoPath;
+            }
             File videoFile = new File(videoPath);
             log.info("You chose " + videoFile + " to upload.");
 
@@ -329,170 +365,5 @@ public class MyDanceService {
             writer.write(credentials.getAccessToken());
             writer.close();
         }
-    }
-
-    // 일치율 계산 부분 (front에서 하기로 해서 안 씀!)
-    private HashMap<String, Object> calculate(Long danceId, String myDanceCoordinatePath) throws IOException {
-        HashMap<String, Object> result = new HashMap<>();
-        ArrayList<Double> matchRates = new ArrayList<Double>();
-        double similaritySum = 0.0;
-
-        /*
-        coordinate에서 result를 가져오는 code
-        String originalDanceJsonFile = originalDanceService.findById(originalDanceId);
-         */
-
-        // 현재는 그냥 더미 데이터로 테스트
-        Reader originalDanceCoordinate = new FileReader("C:\\Users\\SSAFY\\Downloads\\jsonexample.json");
-        Reader myDanceCoordinate = new FileReader(myDanceCoordinatePath);
-
-        JsonParser parser = new JsonParser();
-        JsonArray originalResult = parser.parse(originalDanceCoordinate).getAsJsonArray();
-        JsonArray myResult = parser.parse(myDanceCoordinate).getAsJsonArray();
-
-        // JsonArray에서 하나씩 처리
-        if (myResult.size() > 0) {
-            // 내 챌린지 영상 frame 개수만큼 반복
-            // 원본 영상보다 내 챌린지 영상이 긴 경우는 없다고 가정
-            for (int i = 0; i < myResult.size(); i++) {
-                // keypoints 배열
-                JsonArray originalKeypoints = originalResult.get(i).getAsJsonObject().get("keypoints").getAsJsonArray();
-                JsonArray myKeypoints = myResult.get(i).getAsJsonObject().get("keypoints").getAsJsonArray();
-
-                double similarity = calculate(originalKeypoints, myKeypoints);
-
-                similaritySum += similarity;
-                matchRates.add(similarity);
-
-            }
-        }
-
-        // mycoordinate 파일 삭제
-        myDanceCoordinate.close();
-        fileService.fileDelete(myDanceCoordinatePath);
-
-        result.put("matchRate", matchRates.toString());
-        result.put("score", (int) similaritySum / myResult.size());
-        return result;
-    }
-
-    private double calculate(JsonArray originalKeypoints, JsonArray myKeypoints) {
-        // 관절 벡터 배열
-        int[][] joints = {
-                {8, 12},
-                {7, 11},
-                {8, 7},
-                {12, 14},
-                {16, 14},
-                {16, 20},
-                {16, 18},
-                {11, 13},
-                {13, 15},
-                {15, 19},
-                {15, 17},
-                {12, 11},
-                {12, 24},
-                {11, 23},
-                {24, 23},
-                {24, 26},
-                {26, 28},
-                {28, 32},
-                {28, 30},
-                {23, 25},
-                {25, 27},
-                {27, 29},
-                {27, 31},
-        };
-
-        double sum = 0;
-        double accuracySum = 0;
-
-        for (int[] joint : joints) {
-            JsonObject originalKeypoint1 = originalKeypoints.get(joint[0]).getAsJsonObject();
-            JsonObject originalKeypoint2 = originalKeypoints.get(joint[1]).getAsJsonObject();
-
-            JsonObject myKeypoint1 = myKeypoints.get(joint[0]).getAsJsonObject();
-            JsonObject myKeypoint2 = myKeypoints.get(joint[1]).getAsJsonObject();
-
-            Map<String, Double> originalVector = Map.of(
-                    "x", originalKeypoint1.get("x").getAsDouble() - originalKeypoint2.get("x").getAsDouble(),
-                    "y", originalKeypoint1.get("y").getAsDouble() - originalKeypoint2.get("y").getAsDouble(),
-                    "z", originalKeypoint1.get("z").getAsDouble() - originalKeypoint2.get("z").getAsDouble()
-            );
-
-            Map<String, Double> myVector = Map.of(
-                    "x", myKeypoint1.get("x").getAsDouble() - myKeypoint2.get("x").getAsDouble(),
-                    "y", myKeypoint1.get("y").getAsDouble() - myKeypoint2.get("y").getAsDouble(),
-                    "z", myKeypoint1.get("z").getAsDouble() - myKeypoint2.get("z").getAsDouble()
-            );
-
-            double accuracy = (originalKeypoint1.get("score").getAsDouble() + originalKeypoint2.get("score").getAsDouble()) / 2;
-
-            accuracySum += accuracy;
-
-            // 벡터 정규화
-            Map<String, Double> originalNorm = normalization(originalVector);
-            Map<String, Double> myNorm = normalization(myVector);
-
-            // 코사인 유사도 계산
-            double result = cosineSimilarity(originalNorm, myNorm) * accuracy;
-            sum += result;
-
-        }
-
-        double avg = sum / accuracySum;
-        if (avg < 0)
-            return 0;
-        else
-            return Math.round(avg * 100);
-    }
-
-    private double cosineSimilarity(Map<String, Double> originalNorm, Map<String, Double> myNorm) {
-        if (originalNorm == null || myNorm == null) {
-            throw new IllegalArgumentException("Vectors must not be null");
-        }
-
-        final Set<String> intersection = getIntersection(originalNorm, myNorm);
-
-        final double dotProduct = dot(originalNorm, myNorm, intersection);
-
-        double d1 = 0.0d;
-        for (final Double value : originalNorm.values()) {
-            d1 += Math.pow(value, 2);
-        }
-        double d2 = 0.0d;
-        for (final Double value : myNorm.values()) {
-            d2 += Math.pow(value, 2);
-        }
-        double cosineSimilarity;
-        if (d1 <= 0.0 || d2 <= 0.0) {
-            cosineSimilarity = 0.0;
-        } else {
-            cosineSimilarity = (double) (dotProduct / (double) (Math.sqrt(d1) * Math.sqrt(d2)));
-        }
-        return cosineSimilarity;
-    }
-
-    private Set<String> getIntersection(Map<String, Double> originalNorm, Map<String, Double> myNorm) {
-        final Set<String> intersection = new HashSet<>(originalNorm.keySet());
-        intersection.retainAll(myNorm.keySet());
-        return intersection;
-    }
-
-    private double dot(Map<String, Double> originalNorm, Map<String, Double> myNorm, Set<String> intersection) {
-        double dotProduct = 0;
-        for (final String key : intersection) {
-            dotProduct += originalNorm.get(key) * myNorm.get(key);
-        }
-        return dotProduct;
-    }
-
-    private Map<String, Double> normalization(Map<String, Double> vector) {
-        double norm = Math.sqrt(vector.get("x") * vector.get("x") + vector.get("y") * vector.get("y") + vector.get("z") * vector.get("z"));
-        return Map.of(
-                "x", vector.get("x") / norm,
-                "y", vector.get("y") / norm,
-                "z", vector.get("z") / norm
-        );
     }
 }
