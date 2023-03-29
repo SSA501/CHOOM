eea9abdf4a8a306b33e755aa7526fd6077af57b3
eosun77 <eosun77@gmail.com>
2023-03-29 14:24:00 +0900

    Feat: FileInstance 추가

--- FE/frontend/src/apis/dance.ts
+++ FE/frontend/src/apis/dance.ts
@@ -1,4 +1,4 @@
-import { axiosInstance } from "./instance";
+import { axiosInstance, axiosFileInstance } from "./instance";
 
 // 챌린지 분석 상태 확인 및 변경
 export const getChallengeStatus = async (danceId: string) => {
@@ -8,6 +8,8 @@ export const getChallengeStatus = async (danceId: string) => {
 
 // 챌린지 분석 결과 저장
 export const updateChallenge = async (danceId: string, jsonFile: FormData) => {
-  const response = await axiosInstance.put("/dance/" + danceId, { jsonFile });
+  const response = await axiosFileInstance.put("/dance/" + danceId, {
+    jsonFile,
+  });
   return response.data;
 };
