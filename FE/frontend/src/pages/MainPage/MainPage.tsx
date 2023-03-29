defeff845f2541617c8f95e23d30e5449b37e4dd
김경림 <myway01212@naver.com>
2023-03-29 11:01:39 +0900

    Design: 비디오 캐러셀 버튼 로고로 변경 (#S08P22A501-311)

--- FE/frontend/src/pages/MainPage/MainPage.tsx
+++ FE/frontend/src/pages/MainPage/MainPage.tsx
@@ -3,15 +3,13 @@ import { getPopularChallenge } from "../../apis/challenge";
 import MainBanner from "../../components/MainBanner/MainBanner";
 import ScrollingText from "../../components/ScrollingText/ScrollingText";
 import SearchArea from "../../components/SearchArea/SearchArea";
-import SearchBar from "../../components/SearchBar/SearchBar";
 import VideoCarousel from "../../components/VideoCarousel/VideoCarousel";
 import { TopContainer } from "./style";
 
 export interface VideoDataProps {
-  id: number;
+  youtubeId: number;
   videoPath: string;
   thumbnailPath: string;
-  frameColor?: string;
   title: string;
   url: string;
   userCount: number;
@@ -19,64 +17,13 @@ export interface VideoDataProps {
 }
 
 function MainPage() {
-  const [popularVideoData, setPopularVideoData] = useState<VideoDataProps[]>([
-    {
-      id: 1,
-      videoPath: "/assets/newjeans.mp4",
-      thumbnailPath:
-        "https://i.ytimg.com/vi/SmyFP2MgL4s/hq720_2.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLDXcvDW_CadJyq7wPyRFksAyP0VPQ",
-      title: "하입보이",
-      url: "https://youtu.be/videoId",
-      userCount: 2,
-      status: 2,
-    },
-    {
-      id: 2,
-      videoPath: "/assets/newjeans.mp4",
-      thumbnailPath:
-        "https://i.ytimg.com/vi/SmyFP2MgL4s/hq720_2.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLDXcvDW_CadJyq7wPyRFksAyP0VPQ",
-      title: "하입보이",
-      url: "https://youtu.be/videoId",
-      userCount: 2,
-      status: 2,
-    },
-    {
-      id: 3,
-      videoPath: "/assets/newjeans.mp4",
-      // videoPath: "https://www.youtube.com/shorts/fYQxthUKung",
-      thumbnailPath:
-        "https://i.ytimg.com/vi/SmyFP2MgL4s/hq720_2.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLDXcvDW_CadJyq7wPyRFksAyP0VPQ",
-      title: "하입보이",
-      url: "https://youtu.be/videoId",
-      userCount: 2,
-      status: 2,
-    },
-    {
-      id: 4,
-      videoPath: "/assets/newjeans.mp4",
-      thumbnailPath:
-        "https://i.ytimg.com/vi/SmyFP2MgL4s/hq720_2.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLDXcvDW_CadJyq7wPyRFksAyP0VPQ",
-      title: "하입보이",
-      url: "https://youtu.be/videoId",
-      userCount: 2,
-      status: 2,
-    },
-    {
-      id: 5,
-      videoPath: "/assets/newjeans.mp4",
-      thumbnailPath:
-        "https://i.ytimg.com/vi/SmyFP2MgL4s/hq720_2.jpg?sqp=-oaymwEdCJUDENAFSFXyq4qpAw8IARUAAIhCcAHAAQbQAQE=&rs=AOn4CLDXcvDW_CadJyq7wPyRFksAyP0VPQ",
-      title: "하입보이",
-      url: "https://youtu.be/videoId",
-      userCount: 2,
-      status: 2,
-    },
-  ]);
+  const [popularVideoData, setPopularVideoData] = useState<VideoDataProps[]>(
+    []
+  );
 
   useEffect(() => {
     getPopularChallenge()
       .then((res) => {
-        console.log(res.data);
         setPopularVideoData(res.data);
       })
       .catch((err) => console.log(err));
@@ -95,7 +42,6 @@ function MainPage() {
           "최근 가장 인기있는 챌린지를 모아봤어요 어떤 챌린지를 할지 요즘 가장 인기있는 챌린지를 모아봤어요 어떤 챌린지를 할지 고민된다면 추천해요"
         }
         videoData={popularVideoData}
-        handleBtnClick={() => {}}
       />
     </>
   );
