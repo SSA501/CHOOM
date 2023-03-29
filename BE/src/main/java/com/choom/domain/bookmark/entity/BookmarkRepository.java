4104fe9031b78d181c513165cd9879c4922d4466
yujj25 <yujj25@naver.com>
2023-03-29 09:56:31 +0900

    Update: log 추가, import 정리 (#28, #S08P22A501-334)

--- BE/src/main/java/com/choom/domain/bookmark/entity/BookmarkRepository.java
+++ BE/src/main/java/com/choom/domain/bookmark/entity/BookmarkRepository.java
@@ -1,14 +1,11 @@
 package com.choom.domain.bookmark.entity;
 
-import com.choom.domain.dance.entity.Dance;
-import com.choom.domain.mydance.entity.MyDance;
 import com.choom.domain.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Book;
 import java.util.Optional;
 
 @Repository
