c42efc7d8312ccce0798dbcfb6f15e6084a2c4f3
김경림 <myway01212@naver.com>
2023-03-30 10:43:20 +0900

    Add: 카카오 로그인 리다이렉트 주소 변경 (#S08P22A501-338)

--- FE/frontend/src/apis/user.ts
+++ FE/frontend/src/apis/user.ts
@@ -1,10 +1,11 @@
+import { SERVER_URL } from "../constants/url";
 import { axiosInstance } from "./instance";
 
-// 카카오 코드 받기 위해 리다이렉트
+// 카카오 로그인 리다이렉트
 export const redirectKakao = () => {
-  // 카카오 로그인 주소
   const CLIENT_ID = "113e58b998f18be80dd79db4ef86fee2";
-  const REDIRECT_URI = "http://localhost:3000/login/oauth2/kakao";
+  // const REDIRECT_URI = "http://localhost:3000/login/oauth2/kakao";
+  const REDIRECT_URI = `${SERVER_URL}/login/oauth2/kakao`;
   const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
   window.location.href = KAKAO_AUTH_URL;
 };
