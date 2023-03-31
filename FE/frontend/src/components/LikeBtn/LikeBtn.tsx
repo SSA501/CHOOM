6df1712b3bb3c3800fd9210d68b338baa797381c
김경림 <myway01212@naver.com>
2023-03-31 15:27:44 +0900

    Feat: 비디오 좋아요 기능 (#S08P22A501-400)

--- FE/frontend/src/components/LikeBtn/LikeBtn.tsx
+++ FE/frontend/src/components/LikeBtn/LikeBtn.tsx
@@ -4,35 +4,20 @@ import { ReactComponent as SpeechBubble } from "./speechBubble.svg";
 import { LikeBtnContentContainer, LikeBtnOuterContainer } from "./style";
 
 interface LikeBtnProps {
-  likeCount: number;
+  localLikeCount: number;
   isLiked: boolean | null;
   setIsLiked: (flag: any) => void;
   handleLike: () => void;
   handleLikeDelete: () => void;
 }
 
-/**
- *
- * @param likeCount 좋아요 개수
- * @param isLiked 좋아요 여부 state
- * @param setIsLiked 좋아요 여부 state 변경
- * @param handleLike 좋아요 등록 함수
- * @param handleLikeDelete 좋아요 삭제 함수
- */
-
 function LikeBtn({
-  likeCount,
+  localLikeCount,
   isLiked,
-  setIsLiked,
   handleLike,
   handleLikeDelete,
 }: LikeBtnProps) {
   const fillHeart = isLiked ? "red" : "";
-  const [localLikeCount, setLocalLikeCount] = useState<number>(0);
-
-  useEffect(() => {
-    setLocalLikeCount(likeCount);
-  }, [likeCount]);
 
   return (
     <LikeBtnOuterContainer>
@@ -46,17 +31,10 @@ function LikeBtn({
           onClick={(e) => {
             e.stopPropagation();
             if (isLiked) {
-              handleLike();
-              if (localLikeCount >= 0) {
-                setLocalLikeCount((prev: number) => prev - 1);
-              } else {
-                setLocalLikeCount(0);
-              }
-            } else {
               handleLikeDelete();
-              setLocalLikeCount((prev: number) => prev + 1);
+            } else {
+              handleLike();
             }
-            setIsLiked((prev: boolean) => !prev);
           }}
         >
           {isLiked ? <IoMdHeart /> : <IoMdHeartEmpty />}
