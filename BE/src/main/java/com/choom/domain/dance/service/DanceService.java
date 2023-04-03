2c36babb1ae4bd24754dcd156bc0ffe26e1bdaf7
tykwon97 <tykwon97@naver.com>
2023-04-03 10:58:59 +0900

    Style : Code Convention 수정

--- BE/src/main/java/com/choom/domain/dance/service/DanceService.java
+++ BE/src/main/java/com/choom/domain/dance/service/DanceService.java
@@ -52,25 +52,26 @@ public class DanceService {
 
     private final DanceRepository danceRepository;
     private final MyDanceRepository myDanceRepository;
-    private final BookmarkRepository  bookmarkRepository;
+    private final BookmarkRepository bookmarkRepository;
     private final FileService fileService;
 
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
     private static YouTube youtube;
 
-    private static final String GOOGLE_YOUTUBE_URL =  "https://www.youtube.com/shorts/";
+    private static final String GOOGLE_YOUTUBE_URL = "https://www.youtube.com/shorts/";
     private static final String YOUTUBE_SEARCH_FIELDS1 = "nextPageToken, prevPageToken, pageInfo, items(id/videoId,snippet/title,snippet/channelTitle)";
     private static final String YOUTUBE_SEARCH_FIELDS2 = "items(contentDetails/duration,snippet/title, snippet/description,snippet/publishedAt, snippet/thumbnails/high/url,statistics/likeCount,statistics/viewCount)";
 
-    private static final String SEARCH_SUFFIX  = "#챌린지 #댄스 #쇼츠 #shorts";
+    private static final String SEARCH_SUFFIX = "#챌린지 #댄스 #쇼츠 #shorts";
 
     public static SearchListResponse searchResponse = null;
     public static VideoListResponse videoListResponse = null;
 
     private static String YOUTUBE_APIKEY;
+
     @Value("${apikey.youtube}")
-    public void setKey(String value){
+    public void setKey(String value) {
         YOUTUBE_APIKEY = value;
     }
 
@@ -82,8 +83,8 @@ public class DanceService {
     }
 
     @Transactional
-    public DanceSearchDto searchDance(Long userId, String keyword, String pageToken,Long size) {
-        log.info("Starting YouTube search... " +keyword+" pageToken : "+pageToken);
+    public DanceSearchDto searchDance(Long userId, String keyword, String pageToken, Long size) {
+        log.info("Starting YouTube search... " + keyword + " pageToken : " + pageToken);
         long startTime = System.currentTimeMillis(); // 현재 시간을 밀리초로 가져옵니다.
         long elapsedTime = 0L; // 경과 시간을 초기화합니다.
         long maxTime = 3000L; // 최대 실행 시간을 3초로 설정합니다.
@@ -101,109 +102,112 @@ public class DanceService {
             }
         }
 
-            if (youtube != null) {
-                if(isUrl){
-                    if(keyword.contains("?")){
-                        keyword = keyword.split("\\?")[0];
-                    }
-                    String[] urlList = keyword.split("/");
-                    keyword = urlList[urlList.length-1];
+        if (youtube != null) {
+            if (isUrl) {
+                if (keyword.contains("?")) {
+                    keyword = keyword.split("\\?")[0];
+                }
+                String[] urlList = keyword.split("/");
+                keyword = urlList[urlList.length - 1];
 
-                    log.info("url검색 - keyword : "+keyword);
-                    DanceDetailsDto danceDetailDto = getVideoDetail(userId, keyword);
-                    if(danceDetailDto != null){
-                        Dance dance = danceRepository.findByYoutubeId(keyword).orElse(null);
-                        if(dance == null){ //처음인경우
-                            Dance insertDance = Dance.builder()
-                                .danceDetailDto(danceDetailDto)
-                                .build();
-                            Dance savedDance = danceRepository.save(insertDance);
-                            danceDetailDto.setId(savedDance.getId());
+                log.info("url검색 - keyword : " + keyword);
+                DanceDetailsDto danceDetailDto = getVideoDetail(userId, keyword);
+                if (danceDetailDto != null) {
+                    Dance dance = danceRepository.findByYoutubeId(keyword).orElse(null);
+                    if (dance == null) { //처음인경우
+                        Dance insertDance = Dance.builder()
+                            .danceDetailDto(danceDetailDto)
+                            .build();
+                        Dance savedDance = danceRepository.save(insertDance);
+                        danceDetailDto.setId(savedDance.getId());
+                    }
+                    danceDetailDtoList.add(danceDetailDto);
+                }
+                searchResponse = null;
+            } else {
+                YouTube.Search.List search = null;
+                try {
+                    search = youtube.search().list("snippet");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                search.setKey(YOUTUBE_APIKEY);
+                search.setQ(keyword + " " + SEARCH_SUFFIX);
+                search.setType("video");
+                search.setVideoDuration("short");
+                search.setMaxResults(size);
+                search.setFields(YOUTUBE_SEARCH_FIELDS1);
+                search.setPageToken(pageToken);
+                search.setVideoSyndicated("true");
+
+                try {
+                    searchResponse = search.execute();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                List<SearchResult> searchResultList = searchResponse.getItems();
+
+                if (searchResultList != null) {
+                    for (SearchResult video : searchResultList) {
+                        // 비동기로 검색 -> 검색 속도 향상
+                        String youtubeId = video.getId().getVideoId();
+                        DanceDetailsDto danceDetailDto = getVideoDetail(userId, youtubeId);
+
+                        log.info(video.toString());
+                        if (danceDetailDto != null) {
+                            danceDetailDtoList.add(danceDetailDto);
                         }
-                        danceDetailDtoList.add(danceDetailDto);
-                    }
-                    searchResponse = null;
-                }else{
-                    YouTube.Search.List search = null;
-                    try {
-                        search = youtube.search().list("snippet");
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                    search.setKey(YOUTUBE_APIKEY);
-                    search.setQ(keyword+" "+SEARCH_SUFFIX);
-                    search.setType("video");
-                    search.setVideoDuration("short");
-                    search.setMaxResults(size);
-                    search.setFields(YOUTUBE_SEARCH_FIELDS1);
-                    search.setPageToken(pageToken);
-                    search.setVideoSyndicated("true");
 
-                    try {
-                        searchResponse = search.execute();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-
-                    List<SearchResult> searchResultList = searchResponse.getItems();
-
-                    if (searchResultList != null) {
-                        for (SearchResult video : searchResultList) {
-                            // 비동기로 검색 -> 검색 속도 향상
-                            String youtubeId = video.getId().getVideoId();
-                            DanceDetailsDto danceDetailDto = getVideoDetail(userId, youtubeId);
-
-                            log.info(video.toString());
-                            if (danceDetailDto != null)
-                                danceDetailDtoList.add(danceDetailDto);
-
-                            elapsedTime = System.currentTimeMillis() - startTime; // 경과 시간을 계산합니다.
-                            if (elapsedTime > maxTime) { // 경과 시간이 최대 시간보다 작으면 반복합니다.
-                                log.info("시간초과로 종료됨!");
-                                break;
-                            }
+                        elapsedTime = System.currentTimeMillis() - startTime; // 경과 시간을 계산합니다.
+                        if (elapsedTime > maxTime) { // 경과 시간이 최대 시간보다 작으면 반복합니다.
+                            log.info("시간초과로 종료됨!");
+                            break;
                         }
                     }
                 }
-
             }
 
+        }
 
-
-        Collections.sort(danceDetailDtoList, (o1, o2) -> { //new Comparator<YoutubeResponseDto>() -> lambda
-            // DB에 있는 정보 먼저
-            int id1 = 1;
-            int id2 = 1;
-            if(o1.getId() != null)
-                id1 = 0;
-            if(o2.getId() != null)
-                id2 = 0;
-            if(id1 == id2){
-                // 챌린지 참여자 수 -> 원본 영상 시청자 순 으로 정렬
-                if(o1.getUserCount() == o2.getUserCount()){
-                    return (int)(o2.getViewCount()- o1.getViewCount());
-                }else{
-                    return o2.getUserCount() - o1.getUserCount();
+        Collections.sort(danceDetailDtoList,
+            (o1, o2) -> { //new Comparator<YoutubeResponseDto>() -> lambda
+                // DB에 있는 정보 먼저
+                int id1 = 1;
+                int id2 = 1;
+                if (o1.getId() != null) {
+                    id1 = 0;
                 }
-            }else{
-             return id1-id2;
-            }
-        });
+                if (o2.getId() != null) {
+                    id2 = 0;
+                }
+                if (id1 == id2) {
+                    // 챌린지 참여자 수 -> 원본 영상 시청자 순 으로 정렬
+                    if (o1.getUserCount() == o2.getUserCount()) {
+                        return (int) (o2.getViewCount() - o1.getViewCount());
+                    } else {
+                        return o2.getUserCount() - o1.getUserCount();
+                    }
+                } else {
+                    return id1 - id2;
+                }
+            });
 
         List<DanceDetailsDto> dbDanceDetailDtoList = new ArrayList<>();
-        for (int i=0; i<2; i++){
-            if(danceDetailDtoList.size()<1 || danceDetailDtoList.get(0).getId() == null){
+        for (int i = 0; i < 2; i++) {
+            if (danceDetailDtoList.size() < 1 || danceDetailDtoList.get(0).getId() == null) {
                 break;
             }
             dbDanceDetailDtoList.add(danceDetailDtoList.remove(0));
         }
         DanceSearchDto danceSearchDto = null;
-        if(searchResponse == null){
-            danceSearchDto = new DanceSearchDto(dbDanceDetailDtoList,danceDetailDtoList);
-            log.info("url맞냐1? "+danceSearchDto.getIsUrl());
-        }else{
-            danceSearchDto = new DanceSearchDto(searchResponse,dbDanceDetailDtoList,danceDetailDtoList);
-            log.info("url맞냐2? "+danceSearchDto.getIsUrl());
+        if (searchResponse == null) {
+            danceSearchDto = new DanceSearchDto(dbDanceDetailDtoList, danceDetailDtoList);
+            log.info("url맞냐1? " + danceSearchDto.getIsUrl());
+        } else {
+            danceSearchDto = new DanceSearchDto(searchResponse, dbDanceDetailDtoList,
+                danceDetailDtoList);
+            log.info("url맞냐2? " + danceSearchDto.getIsUrl());
         }
         return danceSearchDto;
     }
@@ -226,20 +230,19 @@ public class DanceService {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        if(videoListResponse.getItems().size() == 0){
+        if (videoListResponse.getItems().size() == 0) {
             //잘못 된 검색인 경우
             return null;
         }
         Video videoDetail = videoListResponse.getItems().get(0);
         //1분 이내 영상인지 확인
         String time = videoDetail.getContentDetails().getDuration();
-        if(time.equals("P0D") || time.contains("M")){ // P0D는 라이브 방송
+        if (time.equals("P0D") || time.contains("M")) { // P0D는 라이브 방송
             return null;
         }
 
-
         Long viewCount = 0L;
-        if(videoDetail.getStatistics().getViewCount()!=null){
+        if (videoDetail.getStatistics().getViewCount() != null) {
             viewCount = videoDetail.getStatistics().getViewCount().longValue();
         }
 
@@ -247,7 +250,7 @@ public class DanceService {
         Dance dance = danceRepository.findByYoutubeId(youtubeId).orElse(null);
 
         Long id = null;
-        if(dance != null){
+        if (dance != null) {
             id = dance.getId();
         }
 
@@ -255,19 +258,19 @@ public class DanceService {
         int status = 0;
         int likeCount = 0;
         Boolean isBookmarked = false;
-        if(dance != null){
+        if (dance != null) {
             userCount = dance.getUserCount();
             status = dance.getStatus();
             likeCount = dance.getBookmarkSize();
-            if(bookmarkRepository.findBookmarkByUserIdAndDanceId(userId, dance.getId()).isPresent()){
+            if (bookmarkRepository.findBookmarkByUserIdAndDanceId(userId, dance.getId())
+                .isPresent()) {
                 isBookmarked = true;
             }
         }
-        String publishedAt = String.valueOf(videoDetail.getSnippet().getPublishedAt()).split("T")[0];
+        String publishedAt = String.valueOf(videoDetail.getSnippet().getPublishedAt())
+            .split("T")[0];
         //1분 이내인 경우
         int s = Integer.parseInt(time.split("T")[1].split("S")[0]);
-
-
 
         DanceDetailsDto danceDetailDto = DanceDetailsDto.builder()
             .id(id)
@@ -287,11 +290,11 @@ public class DanceService {
 
     @Transactional
     public Long addDance(Long userId, String youtubeId) throws IOException {
-        String url = GOOGLE_YOUTUBE_URL+youtubeId;
+        String url = GOOGLE_YOUTUBE_URL + youtubeId;
 
         Dance dance = danceRepository.findByUrl(url).orElse(null);
 
-        if(dance == null) { //처음인경우
+        if (dance == null) { //처음인경우
             DanceDetailsDto danceDetailDto = getVideoDetail(userId, youtubeId);
 
             Dance insertDance = Dance.builder()
@@ -307,10 +310,10 @@ public class DanceService {
     public void saveResult(Long danceId, MultipartFile jsonFile) throws IOException {
         // JSON파일 서버에 저장
         String jsonPath = fileService.fileUpload("coordinate", jsonFile);
-        log.info("변경 된 jsonPath : "+jsonPath);
+        log.info("변경 된 jsonPath : " + jsonPath);
         // DB에 파일 위치 UPDATE
         Dance dance = danceRepository.findById(danceId)
-            .orElseThrow(()->new IllegalArgumentException("존재하지 않는 Dance id값 입니다."));
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Dance id값 입니다."));
         dance.updateJsonPath(jsonPath);
         dance.changeStatus(2); //분석 완료 상태로 변경
     }
@@ -321,7 +324,7 @@ public class DanceService {
             .orElseThrow(() -> new IllegalArgumentException("DB에 없는 Dance Id 입니다!!"));
 
         String youtubeId = dance.getYoutubeId();
-        String url = GOOGLE_YOUTUBE_URL+youtubeId;
+        String url = GOOGLE_YOUTUBE_URL + youtubeId;
 
         // 1. 검색하기 (유튜브API 통해 자세한 동영상 정보 가져오기)
         DanceDetailsDto danceDetailDto = getVideoDetail(userId, youtubeId);
@@ -332,7 +335,7 @@ public class DanceService {
 
         List<DanceRankUserDto> danceRankUserDtoList = new ArrayList<>();
 
-        if(dance == null){ //처음인경우
+        if (dance == null) { //처음인경우
             //3. DB에 저장
             Dance insertDance = Dance.builder()
                 .danceDetailDto(danceDetailDto)
@@ -340,7 +343,7 @@ public class DanceService {
             Dance savedDance = danceRepository.save(insertDance);
             danceDetailDto.setId(savedDance.getId());
 
-        }else{ //처음이 아닌 경우
+        } else { //처음이 아닌 경우
             // 3. 상위 순위 유저 3명 (처음인 경우에는 순위가 0임)
             List<MyDance> myDanceList = myDanceRepository.findRankingUser(dance);
             danceRankUserDtoList = myDanceList.stream().map(myDance ->
@@ -351,7 +354,8 @@ public class DanceService {
                 Collectors.toList());
 
             // 찜한 첼린지 인지 체크하기
-            if(bookmarkRepository.findBookmarkByUserIdAndDanceId(userId, dance.getId()).isPresent()){
+            if (bookmarkRepository.findBookmarkByUserIdAndDanceId(userId, dance.getId())
+                .isPresent()) {
                 danceDetailDto.setBookmark(true);
             }
         }
@@ -368,7 +372,7 @@ public class DanceService {
         List<Dance> danceList = danceRepository.findPopularDance();
         List<PopularDanceDto> popularDanceDtoList = danceList.stream()
             .map(PopularDanceDto::new)
-            .collect( Collectors.toList());
+            .collect(Collectors.toList());
         return popularDanceDtoList;
     }
 
@@ -376,10 +380,10 @@ public class DanceService {
     public DanceStatusDto checkDanceStatus(Long danceId)
         throws YoutubeDLException, UnknownHostException {
         Dance dance = danceRepository.findById(danceId)
-            .orElseThrow(()->new IllegalArgumentException("존재하지 않는 Dance id값 입니다."));
-        int status  = dance.getStatus();
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Dance id값 입니다."));
+        int status = dance.getStatus();
         DanceStatusDto danceStatusDto = null;
-        if(status == 0){ // 분석 안된 상태
+        if (status == 0) { // 분석 안된 상태
             log.info("아직 분석 안 된 영상!!");
             dance.changeStatus(1);
             // 동영상 다운로드
@@ -393,13 +397,13 @@ public class DanceService {
                 .videoPath(videopath)
                 .build();
 
-        }else if(status == 1){ // 분석 중인 상태
+        } else if (status == 1) { // 분석 중인 상태
             log.info("분석 중 인 영상!!"); // 분석 완료 될때까지 기다려야됨???
             danceStatusDto = DanceStatusDto.builder()
                 .videoPath(dance.getVideoPath())
                 .status(0) //나중에 1로 변경해야됨!!!!!!!!!!!!!!
                 .build();
-        }else{ // 분석 완료인 상태
+        } else { // 분석 완료인 상태
             log.info("이미 분석 완료 된 영상!!");
             danceStatusDto = DanceStatusDto.builder()
                 .status(2)
@@ -424,16 +428,17 @@ public class DanceService {
         }
         file = new File(path + youtubeId);
 
-        if (!file.getParentFile().exists())
+        if (!file.getParentFile().exists()) {
             file.getParentFile().mkdirs();
+        }
 
         // Build request
         YoutubeDLRequest request = new YoutubeDLRequest(url, path);
-        request.setOption("ignore-errors");		// --ignore-errors
-        request.setOption("output", "%(id)s.mp4");	// --output "%(id)s"
-        request.setOption("retries", 10);		// --retries 10
+        request.setOption("ignore-errors");        // --ignore-errors
+        request.setOption("output", "%(id)s.mp4");    // --output "%(id)s"
+        request.setOption("retries", 10);        // --retries 10
 
-        YoutubeDL.setExecutablePath(path+"youtube-dl");
+        YoutubeDL.setExecutablePath(path + "youtube-dl");
 
         // Make request and return response
         YoutubeDLResponse response = YoutubeDL.execute(request);
